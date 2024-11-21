c26a0df34fb93acbc7f81512cacfc3b432ad2d1c
bfpill <max@v3rv.com>
2024-11-21 21:36:05 +1100

    Updated Posts

--- app/(posts)/random/[slug]/page.tsx
+++ app/(posts)/random/[slug]/page.tsx
@@ -40,27 +40,63 @@ export function generateMetadata({ params }: PageProps) {
 
 export default function Page({ params }: PageProps) {
   const post = Posts.find((post: { slug: string }) => post.slug === params.slug);
-
+  const title = post ? post.title : "";
+  console.log(post);
   if (!post) {
     notFound();
   }
 
+  console.log("TITLE: ", title)
+
+  let iframeSrc = "https://model-viz.vercel.app";
+
+  if (post.title === "Superposition Viz") {
+    iframeSrc = "https://model-viz.vercel.app/superpos-viz";
+  }
+
+
   return (
-    <div className="absolute top-0 -left-2 flex w-screen h-screen items-center justify-center bg-[#242424] pt-20 md:pt-20">
-      <div className="h-full w-full overflow-y-auto">
-        <div className="absolute top-10 left-10 text-white">
-          <Breadcrumb />
-        </div>
-        <div className="h-full">
-          <div className="flex h-screen w-screen flex-col overflow-auto -mt-32">
-            <iframe
-              src="https://model-viz.vercel.app"
-              className="h-full w-full border-none"
-              title="Embedded Page"
-            />
+    <>
+      {
+        post.title === "Superposition Viz" &&
+        <div className="absolute top-0 -left-2 flex w-screen h-screen items-center justify-center bg-slate-200 pt-20 md:pt-20">
+          <div className="h-full w-full overflow-hidden bg-transparent">
+            <div className="absolute top-10 right-10 text-white w-96">
+              <Breadcrumb />
+            </div>
+            <div className="h-full mt-16">
+              <div className="flex h-screen w-screen flex-col overflow-hidden -mt-32">
+                <iframe
+                  src={iframeSrc}
+                  className="h-full w-full border-none"
+                  title="Embedded Page"
+                />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      }
+
+      {
+        post.title === "Visualizing Neuron Weight Projections" &&
+        <div className="absolute top-0 -left-2 flex w-screen h-screen items-center justify-center bg-[#242424] pt-20 md:pt-20">
+          <div className="h-full w-full overflow-y-auto">
+            <div className="absolute top-10 left-10 text-white">
+              <Breadcrumb />
+            </div>
+            <div className="h-full">
+              <div className="flex h-screen w-screen flex-col overflow-auto -mt-32">
+                <iframe
+                  src={iframeSrc}
+                  className="h-full w-full border-none"
+                  title="Embedded Page"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      }
+    </>
+
   );
 }
