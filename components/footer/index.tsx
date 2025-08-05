466e0cceed87f62c4301af01de62466e56256369
singfluence-anon <max@v3rv.com>
2025-08-05 17:54:53 +1000

    Update index.tsx

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,6 @@
 const Footer = () => {
   return (
     <div className="flex w-full items-center justify-between border-border border-t pt-2">
-      <div className="px-[2px] text-muted text-small">I'm looking for research roles!</div>
       <div className="text-muted text-small">{/* <AppThemeSwitcher /> */}</div>
     </div>
   );
